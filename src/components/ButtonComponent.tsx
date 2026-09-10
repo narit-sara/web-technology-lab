a37378a6a385c80a3ba7c58175f76790c232038c
narit-sara <naritnaphrae@gmail.com>
2026-09-10 13:10:18 +0700

    state-events

--- src/components/ButtonComponent.tsx
+++
@@ -1,8 +0,0 @@
-"use client"; 
-export defult function ButtonComponent(){
-    return(
-        <button type="button" onClick={() => console.log("clicked")}> 
-            ปุ่มทดลอง 
-        </button> 
-    );
-}
